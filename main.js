7a908c4c43d54dabcadf4361a681d9bf42d8165c
rufunt <jazzoo@mail.ru>
2018-11-22 19:27:15 +0300

    Add 'hello world'

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ $(document).ready(function () {
 
 	var tool = new Tool();
 
+	
+
 	tool.onMouseDown = function(event) {
 		var c = Shape.Circle(event.point.x, event.point.y, 30);
 		c.fillColor = 'pink';
 	};
+
+	var c = Shape.Circle(200, 200, 80);
+	c.fillColor = 'black';
+	var text = new PointText(200, 200);
+	text.justification = 'center';
+	text.fillColor = 'white';
+	text.fontSize = 20;
+	text.content = 'hello world';
 
 	paper.view.draw();
 });
